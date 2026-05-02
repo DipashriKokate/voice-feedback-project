5101c90b5d160cc18c4a4799b533385b8afc94b2
Dipashri Kokate <kokatedipashri13@gmail.com>
2026-05-02 15:24:55 +0530

    Added voice recorder frontend

--- frontend/src/components/VoiceRecorder.js
+++ frontend/src/components/VoiceRecorder.js
@@ -1,10 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function VoiceRecorder() {
+  // track recording state
   const [recording, setRecording] = useState(false);
 
-  const toggleRecording = () => {
-    setRecording(!recording);
+  // store recorder and chunks properly
+  const mediaRecorderRef = useRef(null);
+  const audioChunksRef = useRef([]);
+
+  const toggleRecording = async () => {
+
+    // 👉 START RECORDING
+    if (!recording) {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+      const mediaRecorder = new MediaRecorder(stream);
+
+      mediaRecorderRef.current = mediaRecorder;
+      audioChunksRef.current = [];
+
+      mediaRecorder.ondataavailable = (event) => {
+        audioChunksRef.current.push(event.data);
+        console.log("Chunk received");
+      };
+
+      mediaRecorder.onstop = () => {
+        const audioBlob = new Blob(audioChunksRef.current, {
+          type: "audio/wav",
+        }
+       
+      );
+
+        console.log("Final Audio:", audioBlob);
+
+        // play audio
+        const audioUrl = URL.createObjectURL(audioBlob);
+        const audio = new Audio(audioUrl);
+        audio.play();
+         mediaRecorder.stream.getTracks().forEach(track => track.stop());
+      };
+
+      mediaRecorder.start();
+      setRecording(true);
+
+      console.log("Recording started");
+    }
+
+    // 👉 STOP RECORDING
+    else {
+      mediaRecorderRef.current.stop();
+      setRecording(false);
+
+      console.log("Recording stopped");
+    }
   };
 
   return (
